a95d3a85d416128c5302351762fdc5928b20a94a
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2021-02-04 16:21:10 +0900

    Remove version check

--- src/daemon.rs
+++ src/daemon.rs
@@ -303,12 +303,6 @@ impl Daemon {
         };
         let network_info = daemon.getnetworkinfo()?;
         info!("{:?}", network_info);
-        if network_info.version < 16_00_00 {
-            bail!(
-                "{} is not supported - please use bitcoind 0.16+",
-                network_info.subversion,
-            )
-        }
         let blockchain_info = daemon.getblockchaininfo()?;
         info!("{:?}", blockchain_info);
         if blockchain_info.pruned {
