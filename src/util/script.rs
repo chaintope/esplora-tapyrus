f0c65cc7b1bc621d8fd40acc40494cb0e854bb1b
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2021-03-26 15:40:51 +0900

    Add test case

--- src/util/script.rs
+++ src/util/script.rs
@@ -74,3 +74,46 @@ pub fn get_innerscripts(txin: &TxIn, prevout: &TxOut) -> InnerScripts {
         witness_script,
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::chain::Network;
+    use hex::FromHex;
+    use tapyrus::Script;
+
+    fn hex_script(hex: &str) -> Script {
+        return Script::from(Vec::from_hex(hex).unwrap());
+    }
+
+    #[test]
+    fn test_script_to_address() {
+        let script = hex_script("76a91437d8a6977e2b61459c594c8da713a2aeac7516b188ac");
+        let address = script_to_address(&script, Network::new("prod", 1)).unwrap();
+        assert_eq!(address, "166Hhi6oFtpMmugcLY2uyBFDC6caDFjT9s".to_string());
+
+        let script = hex_script("21c13c630f9d53c11847a662c963dfb1e05a8630dcb901262533cb2f590c480cc734bc76a91437d8a6977e2b61459c594c8da713a2aeac7516b188ac");
+        let address = script_to_address(&script, Network::new("prod", 1)).unwrap();
+        assert_eq!(
+            address,
+            "vhUcGQDo5abu4Ld1azfVWyzTDxFzYu9kk93VJ9ozv8zjq8vaGtGemPHKXfRnxPzbNXaxSk5sxd8Neh"
+                .to_string()
+        );
+    }
+
+    #[test]
+    fn test_get_script_type() {
+        let p2pkh = hex_script("76a91437d8a6977e2b61459c594c8da713a2aeac7516b188ac");
+        assert_eq!(get_script_type(&p2pkh), "p2pkh".to_string());
+
+        let p2sh = hex_script("a9143545e6e33b832c47050f24d3eeb93c9c03948bc787");
+        assert_eq!(get_script_type(&p2sh), "p2sh".to_string());
+
+        let cp2pkh = hex_script("21c13c630f9d53c11847a662c963dfb1e05a8630dcb901262533cb2f590c480cc734bc76a91437d8a6977e2b61459c594c8da713a2aeac7516b188ac");
+        assert_eq!(get_script_type(&cp2pkh), "cp2pkh".to_string());
+
+        let cp2sh = hex_script("21c13c630f9d53c11847a662c963dfb1e05a8630dcb901262533cb2f590c480cc734bca9143545e6e33b832c47050f24d3eeb93c9c03948bc787");
+        assert_eq!(get_script_type(&cp2sh), "cp2sh".to_string());
+    }
+}
